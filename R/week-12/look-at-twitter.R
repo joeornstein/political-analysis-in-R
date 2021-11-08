86ed7179fb0d843d7b0f74f2b7da0dd488bc3143
joeornstein <jornstein@gmail.com>
2021-11-08 10:44:22 -0500

    create word cloud

--- R/week-12/look-at-twitter.R
+++ R/week-12/look-at-twitter.R
@@ -31,6 +31,9 @@ tweets <- search_tweets(
   "@terrymcauliffe", n = 200, include_rts = FALSE
 )
 
+# save your tweets if you want
+# save(tweets, file = 'data/raw/terrymcauliffe_tweets.RData')
+
 ## Step 3: Text analysis! -----------------
 
 tidy_tweets <- tweets %>% 
@@ -42,9 +45,20 @@ most_common_words <- tidy_tweets %>%
   count(word) %>% 
   arrange(-n)
 
+# create a word cloud
+library(wordcloud2)
+
+most_common_words %>% 
+  # I'll just keep the words associated with a sentiment
+  inner_join(get_sentiments('bing')) %>% 
+  # wordcloud2 needs a column called 'freq' or it will freak out
+  rename(freq = n) %>% 
+  wordcloud2(color = 'random-dark', size = 0.35)
+
+
 # merge with sentiment lexicon
 tweet_sentiments <- tidy_tweets %>%
-  inner_join(get_sentiments('bing'))
+  left_join(get_sentiments('bing'))
 
 tweet_sentiments %>% 
   count(sentiment)
