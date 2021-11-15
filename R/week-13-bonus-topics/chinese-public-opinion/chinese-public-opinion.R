64581c040bf3fa53599cf9c2b6f3b5028f1a25dd
joeornstein <jornstein@gmail.com>
2021-11-15 14:41:01 -0500

    add chinese public opinion analysis

--- R/week-13-bonus-topics/chinese-public-opinion/chinese-public-opinion.R
+++ R/week-13-bonus-topics/chinese-public-opinion/chinese-public-opinion.R
@@ -33,3 +33,59 @@ wvs %>%
   ggplot(mapping = aes(Q112, y=pct, fill = C_COW_ALPHA)) + 
   geom_col(position = 'dodge')
 
+
+# pride in country (Q254)
+wvs %>% 
+  group_by(C_COW_ALPHA) %>% 
+  count(Q254) %>% 
+  filter(Q254 > 0) %>%
+  mutate(pct = n/sum(n)) %>% 
+  ggplot(mapping = aes(Q254, y=pct, fill = C_COW_ALPHA)) + 
+  geom_col(position = 'dodge')
+
+# most people can be trusted (Q57)
+wvs %>% 
+  group_by(C_COW_ALPHA) %>% 
+  count(Q57) %>% 
+  filter(Q57 > 0) %>%
+  mutate(pct = n/sum(n)) %>% 
+  ggplot(mapping = aes(Q57, y=pct, fill = C_COW_ALPHA)) + 
+  geom_col(position = 'dodge')
+
+# trust people of another religion (Q62)
+wvs %>% 
+  group_by(C_COW_ALPHA) %>% 
+  count(Q62) %>% 
+  filter(Q62 > 0) %>%
+  mutate(pct = n/sum(n)) %>% 
+  ggplot(mapping = aes(Q62, y=pct, fill = C_COW_ALPHA)) + 
+  geom_col(position = 'dodge')
+
+# one of my main goals is to make my parents proud (Q27)
+wvs %>% 
+  group_by(C_COW_ALPHA) %>% 
+  count(Q27) %>% 
+  filter(Q27 > 0) %>%
+  mutate(pct = n/sum(n)) %>% 
+  ggplot(mapping = aes(Q27, y=pct, fill = C_COW_ALPHA)) + 
+  geom_col(position = 'dodge')
+
+# incomes should be made more equal (1)
+# vs. 
+# there should be incentives for greater effort (10)
+wvs %>% 
+  group_by(C_COW_ALPHA) %>% 
+  count(Q106) %>% 
+  filter(Q106 > 0) %>%
+  mutate(pct = n/sum(n)) %>% 
+  ggplot(mapping = aes(Q106, y=pct, fill = C_COW_ALPHA)) + 
+  geom_col(position = 'dodge')
+
+# we depend too much on science and not enough on faith
+wvs %>% 
+  group_by(C_COW_ALPHA) %>% 
+  count(Q160) %>% 
+  filter(Q160 > 0) %>%
+  mutate(pct = n/sum(n)) %>% 
+  ggplot(mapping = aes(Q160, y=pct, fill = C_COW_ALPHA)) + 
+  geom_col(position = 'dodge')
